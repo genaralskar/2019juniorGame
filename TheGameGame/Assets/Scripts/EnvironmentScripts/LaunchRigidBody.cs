85112c10e4a8a2332bec0fab674777a5b6a20d53
Unknown <spencercbarber@gmail.com>
2018-03-29 13:10:28 -0600

    Added "Launch Rigid Body" Script

---
+++ TheGameGame/Assets/Scripts/EnvironmentScripts/LaunchRigidBody.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchPlayer : MonoBehaviour {
+
+//Attach this script to the object with a local Z axis pointing the direction you want to launch a player on contact
+	public float thrust;
+
+    void OnTriggerEnter(Collider other)
+    {
+    other.GetComponent<Rigidbody>().AddForce(transform.forward*thrust);
+    }
+
+}
